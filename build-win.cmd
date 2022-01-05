4de135fe070968dfa5e77d28acb7732b5bdb12cd
Qian Jin <chian.jin@gmail.com>
2022-01-06 00:16:54 +0800

    change icon directory

--- build-win.cmd
+++ build-win.cmd
@@ -1,3 +1,3 @@
 nuitka --show-progress --show-memory --standalone --mingw64 --windows-disable-console^
- --windows-icon-from-ico=src\PDFeXpress.ico --include-data-file=src\PDFeXpress.ico=.\PDFeXpress.ico^
+ --windows-icon-from-ico=src\icon\PDFeXpress.ico --include-data-file=src\icon\PDFeXpress.ico=.\icon\PDFeXpress.ico^
  --plugin-enable=tk-inter --plugin-enable=multiprocessing --output-dir=build src\PDFeXpress.py
