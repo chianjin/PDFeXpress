fc6f40622f7b68f062e2e39f47f57fda05247950
Qian Jin <chian.jin@gmail.com>
2022-01-15 02:22:03 +0800

    fix typo nuitka-build.sh

--- nuitka-build.sh
+++ nuitka-build.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
 python -m nuitka --show-progress --show-memory --standalone \
  --include-data-dir=src/icon=icon --include-data-dir=src/locale=locale \
  --include-data-file=LICENSE=LICENSE --include-data-file=README.md=README.md --include-data-file=README.zh_CN.md=README.zh_CN.md \
  --plugin-enable=tk-inter --plugin-enable=multiprocessing \
- --output-dir=build sr/PDFeXpress.py
+ --output-dir=build src/PDFeXpress.py
