25ad3f3337bad6a7b141f43c885b053be193c378
Qian Jin <chian.jin@gmail.com>
2022-01-16 00:31:59 +0800

    fix bug in font and theme settings

--- src/PDFeXpress.py
+++ src/PDFeXpress.py
@@ -13,16 +13,10 @@ class PDFeXpress(tk.Tk):
     def __init__(self):
         super(PDFeXpress, self).__init__()
 
-        if sys.platform == 'win32':
-            import ctypes
-            ctypes.windll.shcore.SetProcessDpiAwareness(1)
-
         if sys.platform not in ['win32', 'darwin']:
-            style = ttk.Style(self)
-            style.theme_use('clam')
-            for _font in tkfont.names(self):
-                font = tkfont.nametofont(_font, self)
-                font.configure(size=9)
+            ttk.Style(self).theme_use('clam')
+            for font in tkfont.names(self):
+                tkfont.nametofont(font).configure(size=9)
 
         self.wm_geometry(get_geometry(self))
         self.title(APP_NAME)
@@ -34,25 +28,13 @@ class PDFeXpress(tk.Tk):
         self.MainFrame = MainFrame(self)
         self.MainFrame.pack(expand=True, fill='both')
 
-    def set_dpi_awareness(self):
-        if 'win' in sys.platform:
-            import ctypes
-            ctypes.windll.shcore.SetProcessDpiAwareness(1)
-
-    def set_font(self):
-        pass
-
-    def set_style(self):
-        pass
-
     def run(self):
         self.mainloop()
 
 
 if __name__ == '__main__':
-    if 'win' in sys.platform:
+    if sys.platform == 'win32':
         import ctypes
-
         ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
     PDFeXpress().run()
