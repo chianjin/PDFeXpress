6416a359b2c494eaedbf24fb1aa042dd88dc8f58
Qian Jin <chian.jin@gmail.com>
2022-01-16 00:24:15 +0800

    add font and theme setting for Linux, FreeBSD ...

--- src/PDFeXpress.py
+++ src/PDFeXpress.py
@@ -1,5 +1,7 @@
 import sys
 import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.font as tkfont
 
 from app.FrameMenu import FrameMenu
 from app.MainFrame import MainFrame
@@ -11,6 +13,17 @@ class PDFeXpress(tk.Tk):
     def __init__(self):
         super(PDFeXpress, self).__init__()
 
+        if sys.platform == 'win32':
+            import ctypes
+            ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+        if sys.platform not in ['win32', 'darwin']:
+            style = ttk.Style(self)
+            style.theme_use('clam')
+            for _font in tkfont.names(self):
+                font = tkfont.nametofont(_font, self)
+                font.configure(size=9)
+
         self.wm_geometry(get_geometry(self))
         self.title(APP_NAME)
         self.iconphoto(False, tk.PhotoImage(file=APP_ICON))
@@ -20,6 +33,17 @@ class PDFeXpress(tk.Tk):
 
         self.MainFrame = MainFrame(self)
         self.MainFrame.pack(expand=True, fill='both')
+
+    def set_dpi_awareness(self):
+        if 'win' in sys.platform:
+            import ctypes
+            ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+    def set_font(self):
+        pass
+
+    def set_style(self):
+        pass
 
     def run(self):
         self.mainloop()
